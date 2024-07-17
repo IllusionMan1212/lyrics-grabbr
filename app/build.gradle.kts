78a97cdc416ad03fae27655cad3b63173f63330a
IllusionMan1212 <hisham.abourgheba@gmail.com>
2024-07-18 01:07:33 +0200

    v1.1.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.illusionman1212.lyricsgrabbr"
         minSdk = 23
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.0.0"
+        versionCode = 7
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
